ebb296f0e5eb4237b0ec7a6d003be97950f3e7fd
0000username0000 <0aaivanov@gmail.com>
2024-10-16 23:02:07 +0300

    Выведет вопросы и варианты ответов

---
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -0,0 +1,35 @@
+package ru.otus.hw.dao;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.RequiredArgsConstructor;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class CsvQuestionDao implements QuestionDao {
+    private final TestFileNameProvider fileNameProvider;
+
+
+    @Override
+    public List<Question> findAll() {
+        String fileName = fileNameProvider.getTestFileName();
+        InputStream is = getClass().getClassLoader().getResourceAsStream(fileName);
+        if (is == null) {
+            throw new QuestionReadException("CSV file not found: " + fileName);
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        List<QuestionDto> list = new CsvToBeanBuilder<QuestionDto>(reader)
+                .withType(QuestionDto.class)
+                .withSeparator(';')
+                .withSkipLines(1)
+                .build()
+                .parse();
+        return list.stream().map(QuestionDto::toDomainObject).collect(Collectors.toList());
+    }
+}
